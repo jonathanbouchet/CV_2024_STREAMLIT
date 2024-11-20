cec32e9ed14ff343181f005cf5192569ea886f00
Jonathan Bouchet <jonathanbouchet@Jonathans-MacBook-Pro.local>
2024-11-19 16:28:40 -0800

    remmove icons

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ import streamlit as st
 import db
 
 st.set_page_config(layout="wide",
-                   page_title="Jonathan's webpage", page_icon="✅",
-                #    initial_sidebar_state="collapsed",
+                   page_title="Jonathan's webpage",
                    menu_items={
                        'Report a bug': "https://github.com/jonathanbouchet",
                        'Get help':"https://github.com/jonathanbouchet",
@@ -15,11 +14,11 @@ if 'db_initialized' not in st.session_state:
       db.set_db()
       st.session_state["db_initialized"] = True
 
-about_page = st.Page("about.py", title="About Jonathan", icon="❓" )
-resume_page = st.Page("resume.py", title="My Resume", icon="📑")
-portfolio_page = st.Page("portfolio.py", title="My Portfolio", icon="🔢")
-comment_page = st.Page("submit_comment_page.py", title="Comment", icon="🖊️")
-# chatbot_page = st.Page("chatbot.py", title="Ask Me Anything", icon="🤖")
+about_page = st.Page("about.py", title="About Jonathan")
+resume_page = st.Page("resume.py", title="My Resume")
+portfolio_page = st.Page("portfolio.py", title="My Portfolio")
+comment_page = st.Page("submit_comment_page.py", title="Comment")
+# chatbot_page = st.Page("chatbot.py", title="Ask Me Anything")
 
 if __name__ == "__main__":
     pg = st.navigation([about_page, resume_page, portfolio_page, comment_page])
